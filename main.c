c8b05410dbf7490954c139cb6966df002d842f4b
lucas <lucas@localhost.my.domain>
2026-04-10 20:08:57 +0200

    Made it work with a big buffer, should read the messages myself so I can send an appropriately sized buffer or smth

--- main.c
+++ main.c
@@ -8,11 +8,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/types.h>
-#include <netdb.h>
+#include <netdb.h> 
 #include <arpa/inet.h>
 
 
 #define PORT 8080
+#define WEBSITE_PORT 8000
+#define BUFFER_SIZE 1046576
+#define MAX_SOCKET_CONN 5
 
 
 typedef struct
@@ -43,9 +46,10 @@ int opensocket(Socket *newsocket)
 	newsocket->addr.sin_addr.s_addr = INADDR_ANY;
 	newsocket->addr.sin_family = AF_INET;
 
-	if (bind(newsocket->socket, (struct sockaddr *)&newsocket->addr, sizeof(struct sockaddr_in) ) == -1)
+	if (bind(newsocket->socket, (struct sockaddr *)&newsocket->addr, sizeof(newsocket->addr) ) == -1)
 	{
 		perror("Error binding address");
+		close(newsocket->socket);
 		return 1;
 	}
 
@@ -72,11 +76,13 @@ int connecttoapache(uint32_t port, Socket *redirect)
 	if (connect(redirect->socket, (struct sockaddr *)&redirect->addr, sizeof(redirect->addr)) != 0)
 	{
 		perror("Connection failed");
+		close(redirect->socket);
 		return 1;
 	}
 
 	return 0;
 }
+
 
 void closesockets(int32_t *sockets, int32_t socketcount)
 {
@@ -91,49 +97,56 @@ void closesockets(int32_t *sockets, int32_t socketcount)
 
 int main(void)
 {
+	char buffer[BUFFER_SIZE];
 	int32_t clientsocket, errorcode;
-	Socket serversocket, redirectsocket;
-	if (opensocket(&serversocket) == 1)
+	Socket proxysocket, redirectsocket;
+	if (opensocket(&proxysocket) == 1)
 		return 1;	
 
-	if (listen(serversocket.socket, 5) != 0)
-		return 1;
-		
-	int recvres = 0;
-	while (recvres != -1)
+	if (listen(proxysocket.socket, MAX_SOCKET_CONN) < 0)
 	{
-		clientsocket = accept(serversocket.socket, NULL, NULL);
+		close(proxysocket.socket);
+		return 1;
+	}
+
+	while (1)
+	{
+		clientsocket = accept(proxysocket.socket, NULL, NULL);
 		if (clientsocket == -1)
 		{
 			perror("Error connecting to client");
 			errorcode=1;
+			close(clientsocket);
 			break;
 		}
 
 		// Establish a connection to apache
-		int32_t res = connecttoapache(8000, &redirectsocket);
+		int32_t res = connecttoapache(WEBSITE_PORT, &redirectsocket);
 		if (res != 0)
 		{
 			perror("Couldnt connect to apache");
 			errorcode=1;
+			close(clientsocket);
+			close(redirectsocket.socket);
 			break;
 		}
-		
+
 		// Read the request from the client
-		char buffer[65535] = {0};
-		recvres = recv(clientsocket, buffer, sizeof(buffer), 0);
-		if (recvres == 0)
+		int32_t bytes_read = recv(clientsocket, buffer, BUFFER_SIZE, 0);
+		if (bytes_read == 0)
 			continue;
-		if (recvres == -1)
+		if (bytes_read == -1)
 		{
 			perror("Couldnt receive client's request");
 			errorcode=1;
 			break;
 		}
+		if (bytes_read == BUFFER_SIZE)
+			printf("alot of bytes");
 		printf("message from client:\n%s\n", buffer);
 
 		// Send the packet to the server
-		int32_t sendres = send(redirectsocket.socket, buffer, strlen(buffer), 0);
+		int32_t sendres = send(redirectsocket.socket, buffer, bytes_read, 0);
 		if (sendres == 0)
 			continue;
 		if (sendres == -1)
@@ -144,18 +157,20 @@ int main(void)
 		}
 
 		// Receive the reply and send it to the client
-		recvres = recv(redirectsocket.socket, buffer, sizeof(buffer), 0);
-		if (recvres == 0)
+		bytes_read = recv(redirectsocket.socket, buffer, sizeof(buffer), 0);
+		if (bytes_read == 0)
 			continue;
-		if (recvres == -1)
+		if (bytes_read == -1)
 		{
 			perror("Couldnt receive server's answer");
 			errorcode=1;
 			break;
 		}
+		if (bytes_read == BUFFER_SIZE)
+			printf("alot of bytes");
 		printf("message from redirect:\n%s\n", buffer);
 
-		send(clientsocket, buffer, strlen(buffer), 0);
+		send(clientsocket, buffer, bytes_read, 0);
 		if (sendres == 0)
 			continue;
 		if (sendres == -1)
@@ -164,10 +179,13 @@ int main(void)
 			errorcode=1;
 			break;
 		}
+
+		close(clientsocket);
+		close(redirectsocket.socket);
 		
 	}
-
-	closesockets((int []){serversocket.socket, clientsocket, redirectsocket.socket}, 3);
+	
+	closesockets((int []){proxysocket.socket, clientsocket, redirectsocket.socket}, 3);
 
 	return errorcode;
 }
